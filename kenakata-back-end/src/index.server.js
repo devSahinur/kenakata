ef3522482bbb062cdf23f1c33e25a44b68a94115
Sahinur Islam <infosahinur@gmail.com>
2021-08-26 04:26:12 +0600

    new

---
+++ kenakata-back-end/src/index.server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const env = require('dotenv')
+const app = express();
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+//routes
+const userRoutes = require('./routes/user');
+
+
+// environment variable or you can say constants
+env.config();
+
+// mongodb connection
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.1zupo.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    }
+).then(() =>{
+    console.log('Database connected')
+});
+
+// middleware
+app.use(bodyParser());
+app.use('/api', userRoutes);
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
