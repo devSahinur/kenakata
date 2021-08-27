ef762490c9c9a67d1e8ae2c2eb5b816f2f524639
Sahinur Islam <infosahinur@gmail.com>
2021-08-27 19:43:49 +0600

    added sign in and sign up

--- kenakata-admin/src/components/Signin/Signin.js
+++ kenakata-admin/src/components/Signin/Signin.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 const Signin = () => {
     return (
         <Container>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+           <Row className='mt-5'>
+               <Col md={{span:6, offset: 3}}>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                   
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+               </Col>
+           </Row>
         </Container>
     );
 };
--- kenakata-admin/src/components/Signup/Signup.js
+++ kenakata-admin/src/components/Signup/Signup.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 const Signup = () => {
     return (
-        <div>
-            i am sign Up
-        </div>
+        <Container>
+        <Row className='mt-5'>
+            <Col md={{span:6, offset: 3}}>
+             <Form>
+                 <Form.Group className="mb-3" controlId="formBasicEmail">
+                     <Form.Label>Email address</Form.Label>
+                     <Form.Control type="email" placeholder="Enter email" />
+                     <Form.Text className="text-muted">
+                     We'll never share your email with anyone else.
+                     </Form.Text>
+                 </Form.Group>
+
+                 <Form.Group className="mb-3" controlId="formBasicPassword">
+                     <Form.Label>Password</Form.Label>
+                     <Form.Control type="password" placeholder="Password" />
+                 </Form.Group>
+                
+                 <Button variant="primary" type="submit">
+                     Submit
+                 </Button>
+             </Form>
+            </Col>
+        </Row>
+     </Container>
     );
 };
 
