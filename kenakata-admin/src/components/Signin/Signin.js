9e27b2f69260835d88edb49f68cb2f05f9e99506
Sahinur Islam <infosahinur@gmail.com>
2021-08-28 00:32:27 +0600

    added sign in and sign up

--- kenakata-admin/src/components/Signin/Signin.js
+++ kenakata-admin/src/components/Signin/Signin.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import Input from '../Input/Input';
 
 const Signin = () => {
     return (
@@ -7,18 +8,21 @@ const Signin = () => {
            <Row className='mt-5'>
                <Col md={{span:6, offset: 3}}>
                 <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+                        <Input 
+                                label='Email address'
+                                placeholder="Enter your email"
+                                value=""
+                                type="email"
+                                onChange={() => {}}
+                            />
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                        <Input 
+                                label='Password'
+                                placeholder="Enter your password"
+                                value=""
+                                type="password"
+                                onChange={() => {}}
+                            />
                     <Button variant="primary" type="submit">
                         Sign In
                     </Button>
--- kenakata-admin/src/components/Signup/Signup.js
+++ kenakata-admin/src/components/Signup/Signup.js
@@ -12,27 +12,37 @@ const Signup = () => {
                         <Col md={6}>
                             <Input 
                                 label='First Name'
+                                placeholder="First Name"
+                                value=""
+                                type="text"
+                                onChange={() => {}}
                             />
                         </Col>
                         <Col md={6}>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control type="text" placeholder="Last Name" />
-                            </Form.Group>
+                            <Input 
+                                label='Last Name'
+                                placeholder="Last Name"
+                                value=""
+                                type="text"
+                                onChange={() => {}}
+                            />
                         </Col>
                     </Row>
-                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                     <Form.Label>Email address</Form.Label>
-                     <Form.Control type="email" placeholder="Enter email" />
-                     <Form.Text className="text-muted">
-                     We'll never share your email with anyone else.
-                     </Form.Text>
-                 </Form.Group>
+                        <Input 
+                                label='Email address'
+                                placeholder="Enter Your Email"
+                                value=""
+                                type="email"
+                                onChange={() => {}}
+                            />
 
-                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                     <Form.Label>Password</Form.Label>
-                     <Form.Control type="password" placeholder="Password" />
-                 </Form.Group>
+                        <Input 
+                                label='Password'
+                                placeholder="Enter Password"
+                                value=""
+                                type="password"
+                                onChange={() => {}}
+                            />
                 
                  <Button variant="primary" type="submit">
                      Sign Up
